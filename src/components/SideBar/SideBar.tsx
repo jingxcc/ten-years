c413a5441dc8881f3c34c2ed1d34cec108a7bd55
jingxcc <vera.xj.chen@gmail.com>
2023-12-27 03:07:25 +0800

    feat: add Tooltip to replace title attribute

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -16,10 +16,10 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { ReactNode, useEffect } from "react";
 import toast from "react-hot-toast";
+import Tooltip from "../Tooltip/Tooltip";
 
 interface SideBarProps {
   children?: ReactNode;
-  // onSignOut: () => void;
 }
 
 const Sidebar: React.FC<SideBarProps> = ({ children }) => {
@@ -32,9 +32,6 @@ const Sidebar: React.FC<SideBarProps> = ({ children }) => {
       route.push("/");
     } catch (error) {
       if (error instanceof FirebaseError) {
-        // const errorCode = error.code;
-        // const errorMessage = error.message;
-        // const errorMessage = getAuthErrorMsg(error);
         toast.error(`Logout Error: ${error.message}`, {
           position: "top-center",
         });
@@ -44,39 +41,44 @@ const Sidebar: React.FC<SideBarProps> = ({ children }) => {
     }
   };
   return (
-    <div className="w-18 fixed flex h-full flex-col items-center  border-r border-neutral-200 bg-sky-200 p-4">
+    <div className="w-18 fixed z-[50] flex h-full flex-col items-center  border-r border-neutral-200 bg-sky-200 p-4">
       <Image src={"logo.svg"} width={48} height={48} alt="logo"></Image>
       <div className="flex flex-1 flex-col items-center justify-between">
         <div>
-          <button
-            className="btn-icon"
-            title="Chat"
-            onClick={() => route.push("/chat")}
-          >
-            <ChatBubbleLeftEllipsisIcon className="h-8 w-8"></ChatBubbleLeftEllipsisIcon>
-          </button>
-          <button
-            className="btn-icon"
-            title="Suggestions"
-            onClick={() => route.push("/potentials")}
-          >
-            <StarIcon className="h-8 w-8"></StarIcon>
-          </button>
-          <button className="btn-icon" onClick={() => route.push("/likes")}>
-            <EnvelopeIcon className="h-8 w-8" title="Likes You"></EnvelopeIcon>
-          </button>
+          <Tooltip text="Chat">
+            <button className="btn-icon" onClick={() => route.push("/chat")}>
+              <ChatBubbleLeftEllipsisIcon className="h-8 w-8"></ChatBubbleLeftEllipsisIcon>
+            </button>
+          </Tooltip>
+          <Tooltip text="Suggestions">
+            <button
+              className="btn-icon"
+              onClick={() => route.push("/potentials")}
+            >
+              <StarIcon className="h-8 w-8"></StarIcon>
+            </button>
+          </Tooltip>
+          <Tooltip text="Likes You">
+            <button className="btn-icon" onClick={() => route.push("/likes")}>
+              <EnvelopeIcon className="h-8 w-8"></EnvelopeIcon>
+            </button>
+          </Tooltip>
         </div>
         <div>
-          <button
-            className="btn-icon"
-            title="Profile"
-            onClick={() => route.push("/profile")}
-          >
-            <UserCircleIcon className="h-8 w-8"></UserCircleIcon>
-          </button>
-          <button className="btn-icon" title="Log out" onClick={handleSignOut}>
-            <ArrowLeftOnRectangleIcon className="h-8 w-8"></ArrowLeftOnRectangleIcon>
-          </button>
+          <Tooltip text="Profile">
+            <button className="btn-icon" onClick={() => route.push("/profile")}>
+              <UserCircleIcon className="h-8 w-8"></UserCircleIcon>
+            </button>
+          </Tooltip>
+          <Tooltip text="Log out">
+            <button
+              className="btn-icon"
+              title="logouttt"
+              onClick={handleSignOut}
+            >
+              <ArrowLeftOnRectangleIcon className="h-8 w-8"></ArrowLeftOnRectangleIcon>
+            </button>
+          </Tooltip>
         </div>
       </div>
       {children}
